d9586f7a2e07a08236ca90c82cf1ac3f544dc7a7
MichaelEakins <michael.eakins@protonmail.com>
2026-03-01 22:05:27 -0500

    Initial scaffold: solution, project, and test structure

---
+++ src/CrashBytes.Strings/StringExtensions.cs
@@ -0,0 +1,8 @@
+namespace CrashBytes.Strings;
+
+/// <summary>
+/// String extension methods.
+/// </summary>
+public static class StringExtensions
+{
+}
---
+++ tests/CrashBytes.Strings.Tests/StringExtensionsTests.cs
@@ -0,0 +1,10 @@
+namespace CrashBytes.Strings.Tests;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void Placeholder_Test()
+    {
+        Assert.True(true);
+    }
+}
